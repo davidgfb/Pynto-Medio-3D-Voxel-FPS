a2f9040401f727e1c8707f98d025356007c55c10
davidgfb <davidgracia99@gmail.com>
2022-10-23 21:46:20 +0200

    Update Juego.py

--- Juego.py
+++ Juego.py
@@ -1,30 +1,43 @@
 import pygame
 from pygame.mouse import set_pos, set_visible
 from pygame.locals import *
+from pygame.display import set_mode
+from pygame.event import get
+from pygame import init, QUIT, KEYDOWN, K_ESCAPE, K_RETURN, K_PAUSE,\
+                   K_p, MOUSEMOTION
 
 from OpenGL.GL import *
+from OpenGL.GL import glEnable
 from OpenGL.GLU import *
 
-import math
+from numpy import array
 
-pygame.init()
-display = (400, 300)
-scree = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+display = (1280, 720)
+displayCenter = array(set_mode(display, 1073741826).get_size())\
+                // 2
 
-glEnable(GL_DEPTH_TEST)
-glEnable(GL_LIGHTING)
+init()
+
+tuple(map(glEnable, (GL_DEPTH_TEST, GL_LIGHTING,\
+                     GL_COLOR_MATERIAL, GL_LIGHT0))) #opc
+
 glShadeModel(GL_SMOOTH)
-glEnable(GL_COLOR_MATERIAL)
 glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
 
-glEnable(GL_LIGHT0)
-glLightfv(GL_LIGHT0, GL_AMBIENT, [0.5, 0.5, 0.5, 1])
-glLightfv(GL_LIGHT0, GL_DIFFUSE, [1.0, 1.0, 1.0, 1])
+[glLightfv(*a) for a in ((GL_LIGHT0, GL_AMBIENT, array(((1,) * 3 + (2,))) / 2),\
+       (GL_LIGHT0, GL_DIFFUSE, (1,) * 4))] #opc
+
+'''a = array(((GL_LIGHT0, GL_AMBIENT, array(((1,) * 3 + (2,))) / 2),\
+       (GL_LIGHT0, GL_DIFFUSE, (1,) * 4)))
+b, c = a
+
+tuple(map(glLightfv, *(a[:, i] for i in range(len(b))))) #map desordena'''
 
 sphere = gluNewQuadric() 
 
 glMatrixMode(GL_PROJECTION)
-gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+w, h = display
+gluPerspective(45, w / h, 0.1, 50)
 
 glMatrixMode(GL_MODELVIEW)
 gluLookAt(0, -8, 0, 0, 0, 0, 0, 0, 1)
@@ -32,28 +45,30 @@ viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
 glLoadIdentity()
 
 # init mouse movement and center mouse on screen
-displayCenter = [scree.get_size()[i] // 2 for i in range(2)]
+
 mouseMove = [0, 0]
 set_pos(displayCenter)
 set_visible(False)
 
-up_down_angle = 0.0
-paused = False
-run = True
+up_down_angle, paused, run = 0, False, True
+
 while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in get():
+        if event.type == QUIT:
             run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE or event.key == pygame.K_RETURN:
+
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE or event.key == K_RETURN:
                 run = False
-            if event.key == pygame.K_PAUSE or event.key == pygame.K_p:
+            if event.key == K_PAUSE or event.key == K_p:
                 paused = not paused
-                pygame.mouse.set_pos(displayCenter) 
+                set_pos(displayCenter) 
+
         if not paused: 
-            if event.type == pygame.MOUSEMOTION:
-                mouseMove = [event.pos[i] - displayCenter[i] for i in range(2)]
-            pygame.mouse.set_pos(displayCenter)    
+            if event.type == MOUSEMOTION:
+                mouseMove = event.pos - displayCenter
+                
+            set_pos(displayCenter)    
 
     if not paused:
         # get keys
